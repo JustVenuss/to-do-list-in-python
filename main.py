5d5cb4513bdad21ffa32d9495b4ceca6fa3ebd58
venus <venusmabarro@gmail.com>
2024-12-26 09:59:33 +0800

    wrong :( again

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 user_message = "Enter a todo: "
-todo1 = input(user_message)
-todo2 = input(user_message)
-todo3 = input(user_message)
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
 
 todos = [todo1, todo2, todo3]
 print(todos)
